1e644b9a4dc705c16579dee89a90fda82502e840
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2025-04-14 10:02:09 +0800

    增加两个js

---
+++ const pool = mysql.js
@@ -0,0 +1,12 @@
+const pool = mysql.createPool({
+  host: '9.134.107.151',
+  user: 'root',
+  password: 'xuMwn*6829pBfx',
+  port: '3306',
+  database: 'svn_tool',
+  waitForConnections: true,
+  connectionLimit: 10, // 根据实际情况调整
+  queueLimit: 0
+});
+
+这是数据库的信息，这是表tb_branch_info
